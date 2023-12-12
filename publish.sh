786dc3e31bda8d4b371277ba5396324b085a3309
桑榆肖物 <sang93@qq.com>
2023-12-13 00:45:12 +0800

    chore: 更新脚本

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 dotnet publish -c Release -f net8.0 -r win-x64 -o publish/win-x64 -p:PublishSingleFile=true
-dotnet publish -c Release -f net8.0 -r osx-x64 -o publish/win-x64 -p:PublishSingleFile=true
-dotnet publish -c Release -f net8.0 -r linux-x64 -o publish/win-x64 -p:PublishSingleFile=true
+dotnet publish -c Release -f net8.0 -r osx-x64 -o publish/osx-x64 -p:PublishSingleFile=true
+dotnet publish -c Release -f net8.0 -r linux-x64 -o publish/linux-x64 -p:PublishSingleFile=true
